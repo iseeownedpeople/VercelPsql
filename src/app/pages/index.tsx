e95812fd05599b31460d3ee0ec8ae03f31921df9
ESTEBAN FACUNDO <113944973+facundoesteban9@users.noreply.github.com>
2023-10-11 07:16:10 -0300

    c

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,1 +1,16 @@
- 
+import { GetStaticProps } from 'next';  
+import { prisma } from '../lib/prisma';
+export const getStaticProps: GetStaticProps = async () => {
+    const feed = await prisma.post.findMany({
+      where: { published: true },
+      include: {
+        author: {
+          select: { name: true },
+        },
+      },
+    });
+    return {
+      props: { feed },
+      revalidate: 10,
+    };
+  };
